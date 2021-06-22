0334d65fe4fe835b81da85e18f28406d1c765f47
PratikshaLahamage <68214057+PratikshaLahamage@users.noreply.github.com>
2021-06-22 12:18:39 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,21 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+     int  count = 0, a;
+        String x = "";
+  
+        while(n > 0)
+        {
+            a = n % 2;
+            if(a == 1)
+            {
+                count++;
+            }
+            x = a + "" + x;
+            n = n / 2;
+        }
+        
+    }
+    return x;
   }
 }
